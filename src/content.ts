1b71f92068fe922d82b891c3f48dec9dab686714
Caibin Chen <tigersoldi@gmail.com>
2025-02-21 22:52:10 -0800

    add button icons to fav and search (#9)

--- src/content.ts
+++ src/content.ts
@@ -5,6 +5,18 @@ let bibibili: Bibibili;
 
 // Create on document ready
 document.addEventListener("DOMContentLoaded", () => {
-  bibibili = new Bibibili();
   console.log("DOMContentLoaded");
+
+  // Add a CSS to define web font from fonts/promptfont.otf
+  const style = document.createElement("style");
+  style.textContent = `
+    @font-face {
+      font-family: "PromptFont";
+      src: url(${chrome.runtime.getURL("fonts/promptfont.otf")}) format("opentype");
+    }
+  `;
+  document.head.appendChild(style);
+
+  // Initialize the Bibibili instance
+  bibibili = new Bibibili();
 });
